350796c958ac5eadedb33fd465d1b8e2bcd0db87
Gabriel Medeiros da Silva <81653161+Gabrielmsilva96@users.noreply.github.com>
2021-11-09 23:14:03 -0300

    Add files via upload

---
+++ Portifólio_Novo/projetos.js
@@ -0,0 +1,15 @@
+function img01 () {
+    document.getElementById("trocarimg").src="img/img_1_M.png"
+}
+
+function img02 () {
+    document.getElementById("trocarimg").src="img/img_2_M.png"
+}
+
+function img03 () {
+    document.getElementById("trocarimg").src="img/img_3_M.png"
+}
+
+function img04 () {
+    document.getElementById("trocarimg").src="img/img_4_M.png"
+}
